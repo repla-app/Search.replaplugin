d8911ff09f670139a64a4ce308684ad66e65e2e5
Roben Kleene <contact@robenkleene.com>
2018-11-01 20:20:16 -0400

    Update gems

--- Contents/Resources/bundle/bundler/setup.rb
+++ Contents/Resources/bundle/bundler/setup.rb
@@ -3,5 +3,7 @@ require 'rbconfig'
 ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/json-1.8.3/lib"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/webconsole-0.1.19/lib"
+$:.unshift "#{path}/"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/universal-darwin-18/2.3.0/json-2.1.0"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/json-2.1.0/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/webconsole-0.2.0/lib"
