5a114ca581d6f607b51bf128e3cd3717b17b10fa
Roben Kleene <contact@robenkleene.com>
2018-12-18 15:51:00 -0500

    Update gem path

--- Contents/Resources/bundle/bundler/setup.rb
+++ Contents/Resources/bundle/bundler/setup.rb
@@ -6,4 +6,4 @@ path = File.expand_path('..', __FILE__)
 $:.unshift "#{path}/"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/universal-darwin-18/2.3.0/json-2.1.0"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/json-2.1.0/lib"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/webconsole-0.2.0/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/webconsole-0.2.1/lib"
