d00fb3837bc544ca1528f6a7ff235fce6f985920
Roben Kleene <contact@robenkleene.com>
2019-02-16 16:35:54 -0500

    Fix some linter errors

--- Contents/Resources/test/lib/test_parser_inspector.rb
+++ Contents/Resources/test/lib/test_parser_inspector.rb
@@ -1,12 +1,16 @@
-module Repla::Search
-  class Test
-    class ParserInspector
-      def added_file(file)
-        puts 'added_file file = ' + file.inspect
-      end
+module Repla
+  module Search
+    class Test
+      # Parser inspector
+      class ParserInspector
+        def added_file(file)
+          puts "added_file file = #{file.inspect}"
+        end
 
-      def added_line_to_file(line, file)
-        puts 'added_line_to_file line = ' + line.inspect + ' file = ' + file.inspect
+        def added_line_to_file(line, file)
+          puts "added_line_to_file line = #{line.inspect} "\
+            "file = #{file.inspect}"
+        end
       end
     end
   end
