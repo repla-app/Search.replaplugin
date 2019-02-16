ce80ce7b3880443b77b0c56caa2a4879c0745aed
Roben Kleene <contact@robenkleene.com>
2019-02-16 17:06:17 -0500

    Fix some linter errors

--- Contents/Resources/lib/controller.rb
+++ Contents/Resources/lib/controller.rb
@@ -1,21 +1,24 @@
 require_relative 'view'
 require_relative 'model'
 
-module Repla::Search
-  class Controller < Repla::Controller
-    def initialize
-      @view = View.new
-    end
+module Repla
+  module Search
+    # Controller
+    class Controller < Repla::Controller
+      def initialize
+        @view = View.new
+      end
 
-    def added_file(file)
-      @view.add_file(file.file_path, file.display_file_path)
-    end
+      def added_file(file)
+        @view.add_file(file.file_path, file.display_file_path)
+      end
 
-    def added_line_to_file(line, _file)
-      matches = line.matches
-      line_number = line.number
-      text = line.text
-      @view.add_line(line_number, text, matches)
+      def added_line_to_file(line, _file)
+        matches = line.matches
+        line_number = line.number
+        text = line.text
+        @view.add_line(line_number, text, matches)
+      end
     end
   end
 end
