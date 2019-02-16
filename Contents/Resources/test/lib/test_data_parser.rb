1b3b4462cba9e74ed808c6dd13ccdc23d7bea031
Roben Kleene <contact@robenkleene.com>
2019-02-16 16:27:06 -0500

    Fix some linter errors

--- Contents/Resources/test/lib/test_data_parser.rb
+++ Contents/Resources/test/lib/test_data_parser.rb
@@ -3,69 +3,73 @@ require 'json'
 
 require_relative 'test_data_constants'
 
-module Repla::Search
-  module Test
-    module Parser
-      def self.parse(json)
-        hash = JSON.parse(json)
-        get_test_files_hash(hash)
-      end
-
-      # Model
-      class TestFile
-        attr_reader :file_path, :display_file_path, :lines
-        def initialize(file_path, display_file_path = nil)
-          @file_path = file_path
-          @display_file_path = display_file_path || file_path
-          @lines = []
+module Repla
+  module Search
+    module Test
+      # Parser
+      module Parser
+        def self.parse(json)
+          hash = JSON.parse(json)
+          get_test_files_hash(hash)
         end
-        class TestLine
-          attr_reader :number, :matches
-          def initialize(number)
-            @number = number
-            @matches = []
+
+        # Model
+        class TestFile
+          attr_reader :file_path, :display_file_path, :lines
+          def initialize(file_path, display_file_path = nil)
+            @file_path = file_path
+            @display_file_path = display_file_path || file_path
+            @lines = []
           end
-          class TestMatch
-            attr_reader :text
-            def initialize(text)
-              @text = text
+          # Test line
+          class TestLine
+            attr_reader :number, :matches
+            def initialize(number)
+              @number = number
+              @matches = []
+            end
+            # Test match
+            class TestMatch
+              attr_reader :text
+              def initialize(text)
+                @text = text
+              end
             end
           end
         end
-      end
 
-      private
+        private_class_method def self.get_test_files_hash(hashes)
+          test_files_hash = {}
+          test_lines_hash = {}
 
-      def self.get_test_files_hash(hashes)
-        test_files_hash = {}
-        test_lines_hash = {}
+          hashes.each do |hash|
+            file_path = hash[FILE_PATH_KEY]
+            display_file_path = hash[DISPLAY_FILE_PATH_KEY]
+            line_number = hash[LINE_NUMBER_KEY].to_i
+            matched_text = hash[MATCHED_TEXT_KEY]
 
-        hashes.each do |hash|
-          file_path = hash[FILE_PATH_KEY]
-          display_file_path = hash[DISPLAY_FILE_PATH_KEY]
-          line_number = hash[LINE_NUMBER_KEY].to_i
-          matched_text = hash[MATCHED_TEXT_KEY]
+            test_file = test_files_hash[file_path]
+            unless test_file
+              test_file = TestFile.new(file_path, display_file_path)
+              test_files_hash[file_path] = test_file
+              # Create a new test_lines_hash, this will break if our test data
+              # isn't ordered
+              test_lines_hash = {}
+            end
 
-          test_file = test_files_hash[file_path]
-          unless test_file
-            test_file = TestFile.new(file_path, display_file_path)
-            test_files_hash[file_path] = test_file
-            # Create a new test_lines_hash, this will break if our test data isn't ordered
-            test_lines_hash = {}
+            test_line = test_lines_hash[line_number]
+            unless test_line
+              test_line = TestFile::TestLine.new(line_number)
+              test_lines_hash[line_number] = test_line
+              test_file.lines.push(test_line)
+            end
+
+            match = TestFile::TestLine::TestMatch.new(matched_text)
+            test_line.matches.push(match)
           end
 
-          test_line = test_lines_hash[line_number]
-          unless test_line
-            test_line = TestFile::TestLine.new(line_number)
-            test_lines_hash[line_number] = test_line
-            test_file.lines.push(test_line)
-          end
-
-          match = TestFile::TestLine::TestMatch.new(matched_text)
-          test_line.matches.push(match)
+          test_files_hash
         end
-
-        test_files_hash
       end
     end
   end
