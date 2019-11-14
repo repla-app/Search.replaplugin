1bcc6118538c2435250eee039dc875fdfcefb35d
Roben Kleene <contact@robenkleene.com>
2019-11-14 16:28:19 -0500

    Support Catalina (#16)
    * Update shebang line
    
    * Update Ruby
    
    * Update Ruby
    
    * Bump gem

--- Contents/Resources/bundle/bundler/setup.rb
+++ Contents/Resources/bundle/bundler/setup.rb
@@ -4,6 +4,6 @@ ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
 $:.unshift "#{path}/"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/universal-darwin-18/2.3.0/json-2.2.0"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-darwin-18/2.4.0-static/json-2.2.0"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/json-2.2.0/lib"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/repla-0.8.4/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/repla-0.10.4/lib"
